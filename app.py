aac7daf55d58d60376124ae03914ec8a7a091422
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-26 10:20:46 +1200

    worked on arenas page because its very behind

--- app.py
+++ app.py
@@ -24,37 +24,52 @@ def home():
     return render_template("home.html", title="Home")
 
 
-@app.route("/allcards/<int:id>")
-def allcards(id):
-    allcards = connect_database("SELECT id, name, Image FROM Cards WHERE Rarity = ?", (id,))
-    rarity= connect_database("SELECT MAX(id) FROM Rarity")
-    return render_template("allcards.html", title="All Cards", cards=allcards, id=id, rarity=rarity)
 
 
-minimum_id = 1
-maximum_id = 104
+minimum_idarenas = 4
 
-@app.route("/card/<int:id>")
-def card(id):
-    maximum_id = connect_database("SELECT MAX(id) FROM Cards")
-    if id > maximum_id[0][0]:
+@app.route("/arenas/<int:id>")
+def arenas(id):
+    maximum_idarenas = connect_database("SELECT MAX(id) FROM Arena")
+    if id > maximum_idarenas[0][0]:
         abort(404)
-    if id < minimum_id:
+    if id < minimum_idarenas:
         abort(404)
-    card = connect_database("SELECT id, name, Image, description, TypeID FROM Cards WHERE id = ?", (id,))
-    counters = connect_database("SELECT Cards.Name, Cards.Image, Counters.CounterID FROM Counters JOIN Cards ON Counters.CounterID = Cards.id WHERE CardID = ?", (id,))
-    print(card)
-    return render_template("card.html", title=card[0][1], card=card[0], counters=counters, id=id)
-
-
-@app.route("/arenas")
-def arenas():
-    return render_template("arenas.html")
+    return render_template("arenas.html", title=card[0][1])
 
 
 @app.route("/cardtype")
 def cardtype():
     return render_template("cardtype.html")
+
+
+minimum_idcards = 1
+
+@app.route("/allcards/<int:id>")
+def allcards(id):
+    maximum_idcards = connect_database("SELECT MAX(id) FROM Cards")
+    if id > maximum_idcards[0][0]:
+        abort(404)
+    if id < minimum_idcards:
+        abort(404)
+    allcards = connect_database("SELECT id, name, Image FROM Cards WHERE Rarity = ?", (id,))
+    rarity= connect_database("SELECT MAX(id) FROM Rarity")
+    return render_template("allcards.html", title="All Cards", cards=allcards, id=id, rarity=rarity)
+
+
+minimum_idcards = 1
+
+@app.route("/card/<int:id>")
+def card(id):
+    maximum_idcards = connect_database("SELECT MAX(id) FROM Cards")
+    if id > maximum_idcards[0][0]:
+        abort(404)
+    if id < minimum_idcards:
+        abort(404)
+    card = connect_database("SELECT id, name, Image, description, TypeID FROM Cards WHERE id = ?", (id,))
+    counters = connect_database("SELECT Cards.Name, Cards.Image, Counters.CounterID FROM Counters JOIN Cards ON Counters.CounterID = Cards.id WHERE CardID = ?", (id,))
+    print(card)
+    return render_template("card.html", title=card[0][1], card=card[0], counters=counters, id=id)
 
 
 @app.errorhandler(404)

e7ec53e2d24f3c49e6b717d9c8c6728faa3b22e6
Parthgaik <parthgaik@gmail.com>
2024-09-26 09:22:57 +1200

    code comenting and documentation testing

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ def connect_database(statement, id=None):
     '''Connection and cursor connect to reduce repeated code'''
     conn = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = conn.cursor()
+    #
     if id is not None:
         cursor.execute(statement, id)
     else:
@@ -19,18 +20,20 @@ def connect_database(statement, id=None):
     return results
 
 
-
 @app.route("/")
 def home():
+    '''Home page'''
     return render_template("home.html", title="Home")
+
 
 minimum_idarenas = 4
 
+
 @app.route("/arenas")
 def arenas():
+    '''Displaying all arenas page'''
     arenas = connect_database("SELECT id, name, Image FROM Arena")
     return render_template("arenas.html", title="Arenas", arenas=arenas)
-
 
 
 minimum_idcardtype = 1
@@ -38,12 +41,14 @@ minimum_idcardtype = 1
 
 @app.route("/cardtype")
 def cardtype():
+    '''All cardtypes page'''
     cardtype = connect_database("SELECT id, name, Description, Image FROM Type")
     return render_template("cardtypes.html", title = "Card Types", cardtype=cardtype)
 
 
 @app.route("/type/<int:id>")
 def type(id):
+    '''Type of card page'''
     type = connect_database("SELECT * FROM Type WHERE id = ?", (id,))
     return render_template("type.html",title = type[0][1] ,type = type[0])
 
@@ -52,6 +57,7 @@ minimum_idcards = 1
 
 @app.route("/allcards/<int:id>")
 def allcards(id):
+    '''Displays all cards, error handler applied with the edition of rarity to sort cards by pages so not all the cards are all on one page'''
     maximum_idcards = connect_database("SELECT MAX(id) FROM Rarity")
     if id > maximum_idcards[0][0]:
         abort(404)
@@ -66,6 +72,7 @@ minimum_idcards = 1
 
 @app.route("/card/<int:id>")
 def card(id):
+    '''Error handler on single card page, Selecting certain information from card and taking the card counters and joining it into this page'''
     maximum_idcards = connect_database("SELECT MAX(?) FROM Cards", (id,))
     if id > maximum_idcards[0][0]:
         abort(404)
@@ -79,8 +86,8 @@ def card(id):
 
 @app.errorhandler(404)
 def pagenotfound(e):
-    return render_template("/404.html", error=e),404
-
+    '''Error handler'''
+    return render_template("/404.html", error=e), 404
 
 
 if __name__ == "__main__":

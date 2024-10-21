9474b1f7e7bd7bdb8c2fbfa3364c1c2e927246f6
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-10-22 10:16:03 +1300

    code commenting

--- app.py
+++ app.py
@@ -10,7 +10,8 @@ def connect_database(statement, id=None, fetch_all=True):
     '''Connection and cursor connect to reduce repeated code'''
     conn = sqlite3.connect("Yr13ClashRoyaleDB.db")
     cursor = conn.cursor()
-    #
+    # When an id is given to the function, the results are fetched through either a fetchall or fetchone
+    # Making an if fetch_all loop in case both fetchall or fetchone are required
     if id is not None:
         cursor.execute(statement, id)
     else:
@@ -18,20 +19,21 @@ def connect_database(statement, id=None, fetch_all=True):
     if fetch_all:
         results = cursor.fetchall()
     else:
-        results =  cursor.fetchone()
-
+        results = cursor.fetchone()
     conn.close()
     return results
 
 
 def get_max_id(table_name):
+    '''Finding the maximum id of a query'''
+    # Using the result of the function, turing it into a string using the f string and running it through the query
+    # Making sure there is actually a result from the if result[0] loop
     query = f"SELECT MAX(id) FROM {table_name}"
     result = connect_database(query, fetch_all=False)
     if result[0] is not None:
         return result[0]
     else:
         return 0
-
 
 
 @app.route("/")
@@ -46,6 +48,7 @@ minimum_idarenas = 4
 @app.route("/arenas")
 def arenas():
     '''Displaying all arenas page'''
+    # Selecting all items from the id, name, image columns from the arena table into the query and displaying it onto the website
     arenas = connect_database("SELECT id, name, Image FROM Arena")
     return render_template("arenas.html", title="Arenas", arenas=arenas)
 
@@ -56,6 +59,8 @@ minimum_idcardtype = 1
 @app.route("/cardtype")
 def cardtype():
     '''All cardtypes page'''
+    # Selecting all items from the id, name, image, description columns from the type 
+    # table into the query and displaying it onto the website
     cardtype = connect_database("SELECT id, name, Description, Image FROM Type")
     return render_template("cardtypes.html", title = "Card Types", cardtype=cardtype)
 
@@ -63,6 +68,10 @@ def cardtype():
 @app.route("/type/<int:id>")
 def type(id):
     '''Type of card page'''
+    # Error prevention in function
+    # Using the get_max_id function for type, seeing if the value of it is larger than the id retrieved from the user, if so, abort the 404 page
+    # otherwise return the items within the range onto the webpage
+    # 'If not type' is for if id retireved is within the range of maximum id but does not exist in database
     max_id = get_max_id("Type")
     if id > max_id:
         abort(404)
@@ -77,6 +86,11 @@ minimum_idcards = 1
 @app.route("/allcards/<int:id>")
 def allcards(id):
     '''Displays all cards, error handler applied with the edition of rarity to sort cards by pages so not all the cards are all on one page'''
+    # Error prevention in function
+    # The query with the rarity is for the sorting of my items on the webpage, decided to sort the items by rarity
+    # Using the get_max_id function for rarity, seeing if the value of it is larger than the id retrieved from the user, if so, abort the 404 page
+    # otherwise return the items within the range onto the webpage
+    # 'If not type' is for if id retireved is within the range of maximum id but does not exist in database
     maximum_idcards = connect_database("SELECT MAX(id) FROM Rarity")
     if id > maximum_idcards[0][0]:
         abort(404)
@@ -87,10 +101,13 @@ def allcards(id):
     return render_template("allcards.html", title="All Cards", cards=allcards, id=id, rarity=rarity)
 
 
-
 @app.route("/card/<int:id>")
 def card(id):
     '''Error handler on single card page, Selecting certain information from card and taking the card counters and joining it into this page'''
+    # Error prevention in function
+    # Using the get_max_id function for Cards, seeing if the value of it is larger than the id retrieved from the user, if so, abort the 404 page
+    # otherwise return the items within the range onto the webpage
+    # counters variable is for displaying all the counters for a certain card, joining other parts of tables onto the query
     max_id = get_max_id("Cards")
     if id > max_id:
         abort(404)
@@ -98,15 +115,15 @@ def card(id):
     if not card:
         abort(404)
     counters = connect_database("SELECT Cards.Name, Cards.Image, Counters.CounterID FROM Counters JOIN Cards ON Counters.CounterID = Cards.id WHERE CardID = ?", (id,))
-    print(card)
     return render_template("card.html", title=card[0][1], card=card[0], counters=counters, id=id)
 
 
 @app.errorhandler(404)
 def pagenotfound(e):
     '''Error handler'''
+    # Handles errors
     return render_template("/404.html", error=e), 404
 
-
+# :)
 if __name__ == "__main__":
     app.run(debug=True)
